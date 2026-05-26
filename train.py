8f11868bf4b1c6f60452aa6665ef7d908af712f0
BoarArtem <workboar@gmail.com>
2026-05-26 18:52:28 +0000

    model saved

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     model = YOLO("yolo26n.pt")
 
-    model.train(data="dataset/data.yaml", epochs=15)
+    model.train(data="dataset/data.yaml", epochs=50)
